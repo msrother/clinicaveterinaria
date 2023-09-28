1690235e042b83142cbd54c6b7e6c2ceda59101c
msrother <somenzi.rother@gmail.com>
2023-09-27 22:13:46 -0300

    Commit inicial

---
+++ src/main/kotlin/br/upf/clinicaveterinaria/ClinicaveterinariaApplication.kt
@@ -0,0 +1,11 @@
+package br.upf.clinicaveterinaria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClinicaveterinariaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClinicaveterinariaApplication>(*args)
+}
---
+++ src/test/kotlin/br/upf/clinicaveterinaria/ClinicaveterinariaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.upf.clinicaveterinaria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClinicaveterinariaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
